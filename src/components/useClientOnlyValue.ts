571f1fbc266d9dfa5d1818fc95ac20fe4dd64de8
Hassam Ullah <malikhassamullah1@yahoo.com>
2024-02-23 18:37:31 +0500

    chore(general): add dummy data

--- src/components/useClientOnlyValue.ts
+++ src/components/useClientOnlyValue.ts
@@ -1,6 +1,4 @@
 // This function is web-only as native doesn't currently support server (or build-time) rendering.
-function useClientOnlyValue<S, C>(server: S, client: C): S | C {
+export function useClientOnlyValue<S, C>(server: S, client: C): S | C {
   return client;
 }
-
-export { useClientOnlyValue };
